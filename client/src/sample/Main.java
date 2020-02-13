10f423045414870f2215c56b432ae8da66bfa276
cecile_besson <cecile.besson@live.fr>
2020-02-13 20:06:01 +0100

    Work in progress: manage authentification and communication to the server

--- client/src/sample/Main.java
+++ client/src/sample/Main.java
@@ -1,6 +1,6 @@
 package sample;
 
-import javafx.application.Application;
+/*import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
@@ -20,4 +20,4 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-}
+}*/
---
+++ server/src/com/company/Connexion.java
@@ -0,0 +1,146 @@
+package com.company;
+
+import com.company.security.PopSecurity;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.Socket;
+import java.net.SocketException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Connexion implements Runnable {
+    private Socket socket;
+    private BufferedOutputStream writer;
+    private BufferedInputStream reader;
+    private boolean authentified = false;
+    private boolean closeConnexion = false;
+    private HashMap<String, String> lpasswd;
+
+
+    public Connexion(Socket socket, HashMap<String, String> lpasswd) throws IOException {
+        if (socket == null) {
+            throw new IllegalArgumentException("instance of Socket can not be null");
+        }
+        this.socket = socket;
+        this.lpasswd = lpasswd;
+        writer = new BufferedOutputStream(socket.getOutputStream());
+        reader = new BufferedInputStream(socket.getInputStream());
+
+    }
+
+    public void run() {
+        System.err.println("Lancement du processus d'authentification");
+        String command = null;
+        while (!socket.isClosed()) {
+            try {
+                //on attend une commande du client
+                System.out.println("WAIT");
+                command = read();
+                System.out.println("READ " + command + " " + authentified);
+                List<String> lcommand = explode(command);
+                String response = "";
+                if (!authentified) {
+                    response = authentification(lcommand, response);
+                    if (response.equalsIgnoreCase("+OK")) {
+                        authentified = true;
+                    }
+
+                } else response = communication(lcommand, response);
+                //On envoie la réponse au client
+                writer.write(response.getBytes());
+                writer.flush();
+
+
+                if (closeConnexion) {
+                    System.err.println("Deconnexion");
+                    socket.close();
+                }
+            } catch (SocketException e) {
+                System.err.println("Connexion interrompue");
+            } catch (IOException | NoSuchAlgorithmException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private List<String> explode(String command) {
+        List<String> lcommande = new ArrayList<String>();
+        StringTokenizer st = new StringTokenizer(command, " ");
+        while (st.hasMoreElements()) {
+            lcommande.add((String) st.nextElement());
+        }
+        return lcommande;
+    }
+
+    private String read() throws IOException {
+        String clientCommand = "";
+        int stream;
+        byte[] b = new byte[4096];
+        stream = reader.read(b);
+        clientCommand = new String(b, 0, stream);
+        clientCommand = clientCommand.replaceAll("[\r\n]+", "");
+        return clientCommand;
+    }
+
+    public String authentification(List<String> lcommand, String response) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        String command = lcommand.get(0);
+        switch (command.toUpperCase()) {
+            case "APOP":
+
+                String user = lcommand.get(1);
+                String passwd = lcommand.get(2);
+                if (lpasswd.get(user) != null && lpasswd.get(user).equalsIgnoreCase(PopSecurity.getMd5String(passwd))) {
+                    authentified = true;
+                    response = "+OK";
+                } else {
+                    authentified = false;
+                    response = "-ERR";
+                }
+
+                break;
+            case "QUIT":
+                //traitement cas quit
+                closeConnexion = true;
+                response = "+OK";
+                break;
+            default:
+                //traitement cas default
+                response = "Commande inconnue ou identification non effectuee";
+                break;
+        }
+        return response;
+    }
+
+    public String communication(List<String> lcommand, String response) {
+        String command = lcommand.get(0);
+
+        switch (command.toUpperCase()) {
+            case "STAT":
+                //traitement stat
+                break;
+            case "LIST":
+                //traitement list
+                break;
+            case "RETR":
+                //traitment retr
+                break;
+            case "QUIT":
+                closeConnexion = true;
+                response = "+OK";
+                break;
+            default:
+                response = "Commande inconnue ou identification non effectuee";
+                break;
+
+
+        }
+        return response;
+    }
+
+}
