61cff890053c20855eabf61fbf2c69faffd6174d
Thibaut <t.delplanque92@gmail.com>
2020-02-23 18:04:01 +0100

    FIX: fix read method + comments

--- server/src/poly/Connexion.java
+++ server/src/poly/Connexion.java
@@ -10,20 +10,41 @@ import java.io.UnsupportedEncodingException;
 import java.net.Socket;
 import java.net.SocketException;
 import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
 
 public class Connexion implements Runnable {
+
+    // TODO Maybe replace by System.lineSeparator()
+    private static final String CARRIAGE_RETURN = "\r\n";
+
+    private static final String CODE_OK = "+OK";
+    private static final String CODE_ERR = "-ERR";
+
+    private static final String SERVER_READY_MSG = CODE_OK + " Server ready";
+    private static final String DEFAULT_ERR_MSG = CODE_ERR + " Unknown command or authentication failed";
+
+
+    /** boolean value, represents the state of the user, authenticated or not */
+    private boolean authenticated = false;
+    /** boolean value, represents the state of the connexion */
+    private boolean closeConnexion = false; // TODO USELESS ??
+
+    /** Socket instance for connexion */
     private Socket socket;
     private BufferedOutputStream writer;
     private BufferedInputStream reader;
-    private boolean authentified = false;
-    private boolean closeConnexion = false;
 
+    /**
+     * user's mailbox, it is instantiated only if the user is authenticated and if he has a mailbox
+     * if authentication fails, then server return -ERR to client
+     * if user's mailbox doesn't exist, then create a new folder
+     */
     private Mailbox mailBox;
 
-
+    /**
+     * create a new instance of connexion
+     * @param socket used for the connexion
+     * @throws IOException, only if the socket instance is null
+     */
     public Connexion(Socket socket) throws IOException {
         if (socket == null) {
             throw new IllegalArgumentException("instance of Socket can not be null");
@@ -31,130 +52,117 @@ public class Connexion implements Runnable {
         this.socket = socket;
         writer = new BufferedOutputStream(socket.getOutputStream());
         reader = new BufferedInputStream(socket.getInputStream());
-
-        // TODO get global path + userName
-        // this.mailBox = new MailBox(this.l)
-
     }
 
+    // TODO review the run method, if we can structure it better
     public void run() {
-        System.err.println("Lancement du processus d'authentification");
-        String command = "null";
-        String response = "+OK: server ready";
+        System.err.println("Launch of the authentication process");
+        Command command;
+        String answer;
         try {
-            writer.write((response + "\n").getBytes());
+            writer.write((SERVER_READY_MSG + CARRIAGE_RETURN).getBytes());
             writer.flush();
             while (!socket.isClosed()) {
-
-                //on attend une commande du client
-                System.out.println("WAIT");
                 try {
-                    command = read();
+                    // waiting for Client action
+                    System.out.println("WAIT");
+                    command = new Command(read());
+                    System.out.printf("AUTH: %s CMD: %s%n", authenticated, command);
 
-                    System.out.println("READ " + command + " " + authentified);
-                    List<String> lcommand = explode(command);
-                    if (!authentified) {
-                        response = authentification(lcommand, response);
-                        if (response.equalsIgnoreCase("+OK")) {
-                            authentified = true;
-                        }
+                    if (!authenticated) {
+                        answer = authentication(command);
+                        // TODO treatment in the wrong place
+                        if (answer.equalsIgnoreCase("+OK"))
+                            authenticated = true;
 
-                    } else response = communication(lcommand, response);
-                    //On envoie la réponse au client
-                    writer.write((response + "\n").getBytes());
+                    } else answer = communication(command);
+
+                    // We send the response back to the client
+                    writer.write((answer + "\n").getBytes());
                     writer.flush();
-
-
                     if (closeConnexion) {
-                        System.err.println("Deconnexion");
+                        System.err.println("Disconnection");
                         socket.close();
                     }
                 } catch (IndexOutOfBoundsException e) {
-                    System.err.println("Pas de commande saisie");
+                    System.err.println("No command entered");
                 }
             }
         } catch (SocketException e) {
-            System.err.println("Connexion interrompue");
+            System.err.println("Connection interrupted");
         } catch (IOException | NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
     }
 
-
-
-    private List<String> explode(String command) {
-        List<String> lcommande = new ArrayList<String>();
-        StringTokenizer st = new StringTokenizer(command, " ");
-        while (st.hasMoreElements()) {
-            lcommande.add((String) st.nextElement());
-        }
-        return lcommande;
-    }
-
+    // TODO Check whether the read method respects all message forms
+    /**
+     * The read method, reads incoming commands on the socket from the client
+     * @return, returns a String containing the client's command
+     * @throws IOException
+     */
     private String read() throws IOException {
-        String clientCommand = "";
+        StringBuilder builder = new StringBuilder();
+        String buffer;
         int stream;
-        byte[] b = new byte[4096];
-        stream = reader.read(b);
-        clientCommand = new String(b, 0, stream);
-        clientCommand = clientCommand.replaceAll("[\r\n]+", "");
-        return clientCommand;
+        do {
+            byte[] b = new byte[4096];
+            stream = reader.read(b);
+            buffer = new String(b, 0, stream);
+            builder.append(buffer);
+        } while(!buffer.contains(CARRIAGE_RETURN));
+        return builder.toString();
     }
 
-    public String authentification(List<String> lcommand, String response) throws UnsupportedEncodingException, NoSuchAlgorithmException {
-        String command = lcommand.get(0);
-        switch (command.toUpperCase()) {
-            case "APOP":
-
-                String user = lcommand.get(1);
-                String passwd = lcommand.get(2);
-                if (UserHandler.checkAuth(user, passwd)) {
-                    authentified = true;
-                    response = "+OK";
+    public String authentication(Command command) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        String answer;
+        switch (command.getCommand()) {
+            case Command.APOP:
+                String user = command.getParam(0);
+                String hash = command.getParam(1);
+                if (UserHandler.checkAuth(user, hash)) {
+                    authenticated = true;
+                    answer = CODE_OK;
                 } else {
-                    authentified = false;
-                    response = "-ERR";
+                    authenticated = false;
+                    answer = CODE_ERR;
                 }
-
                 break;
-            case "QUIT":
-                //traitement cas quit
+            case Command.QUIT:
+                // AUTH QUIT Scenario
                 closeConnexion = true;
-                response = "+OK";
+                answer = CODE_OK;
                 break;
             default:
-                //traitement cas default
-                response = "-ERR : Commande inconnue ou identification non effectuee";
-
+                // AUTH Default scenario
+                answer = DEFAULT_ERR_MSG;
                 break;
         }
-        return response;
+        return answer;
     }
 
-    public String communication(List<String> lcommand, String response) {
-        String command = lcommand.get(0);
-
-        switch (command.toUpperCase()) {
-            case "STAT":
-                //traitement stat
+    public String communication(Command command) {
+        String answer = "";
+        switch (command.getCommand()) {
+            case Command.STAT:
+                // TODO STAT implementation to do
                 break;
-            case "LIST":
-                //traitement list
+            case Command.LIST:
+                // TODO LIST implementation to do
                 break;
-            case "RETR":
-                //traitment retr
+            case Command.RETR:
+                // TODO RETR implementation to do
                 break;
-            case "QUIT":
+            case Command.QUIT:
                 closeConnexion = true;
-                response = "+OK";
+                // TODO answer not precise enough
+                answer = CODE_OK;
                 break;
             default:
-                response = "Commande inconnue ou identification non effectuee";
+                answer = DEFAULT_ERR_MSG;
                 break;
-
-
         }
-        return response;
+        return answer;
     }
 
 }
